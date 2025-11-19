9860672ad227513004a2f78ccf69605225d9fffa
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2025-11-19 22:00:41 +0100

    feat: setup drizzle

---
+++ db/schema.ts
@@ -0,0 +1,8 @@
+import { text, sqliteTable } from "drizzle-orm/sqlite-core";
+
+export const userTable = sqliteTable("user", {
+    id: text("id").primaryKey(),
+    name: text("name").notNull(),
+    email: text("email").notNull(),
+
+});
