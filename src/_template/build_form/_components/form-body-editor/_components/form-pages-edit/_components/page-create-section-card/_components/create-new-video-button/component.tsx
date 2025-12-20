f0b6676ad8b3f818fcf37121ce1d1b2af1dedb30
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2025-12-20 21:38:36 +0100

    fix: movie icon change in new section component

--- src/_template/build_form/_components/form-body-editor/_components/form-pages-edit/_components/page-create-section-card/_components/create-new-video-button/component.tsx
+++ src/_template/build_form/_components/form-body-editor/_components/form-pages-edit/_components/page-create-section-card/_components/create-new-video-button/component.tsx
@@ -3,7 +3,7 @@ import CreateVideoAction from '@/_server/_handlers/actions/video/create'
 import { CreateNewSectionButtonComponent } from '@/_template/build_form/_components/form-body-editor/_components/form-pages-edit/_components/page-create-section-card/_components/shared/create-new-section-button/component'
 import { ICreateNewSectionButtonProps } from '@/_template/build_form/_components/form-body-editor/_components/form-pages-edit/_components/page-create-section-card/_components/shared/types'
 import { useAfterSaveNewSectionOptimistic } from '@/_template/build_form/_components/form-body-editor/_components/form-pages-edit/_components/page-create-section-card/hooks/useAfterSaveNewSectionOptimistic'
-import { ImageIcon } from 'lucide-react'
+import { ClapperboardIcon } from 'lucide-react'
 
 export function CreateNewVideoButtonComponent(
     props: ICreateNewSectionButtonProps
@@ -17,7 +17,7 @@ export function CreateNewVideoButtonComponent(
     return (
         <CreateNewSectionButtonComponent
             buttonText="Nuevo Video"
-            buttonIcon={<ImageIcon className="w-10! h-10!" />}
+            buttonIcon={<ClapperboardIcon className="w-10! h-10!" />}
             dialogTitle="Crear nuevo video"
             serverAction={CreateVideoAction}
             afterSave={afterSave}
